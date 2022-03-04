0ed8fa11cf8cfb0eee3b8fddc1e1195fde10f58b
Mihai Dornescu <dornescu@gmail.com>
2022-03-04 15:03:01 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        'barlow': ['"Barlow"', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
